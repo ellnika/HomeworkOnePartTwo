fa618ce9f05731522e8ef15bb17beaf11a6e4f76
Ewa Czekalska <Ewa@Ewas-MacBook-Air.local>
2016-08-08 11:49:20 +0100

    first commit

--- HomeworkOne-Project2/ViewController.m
+++ HomeworkOne-Project2/ViewController.m
@@ -10,9 +10,26 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *inputField;
+
 @end
 
 @implementation ViewController
+
+- (IBAction)buttonClickedCheck:(id)sender {
+    
+    NSString *input;
+    input=self.inputField.text;
+    NSInteger length = input.length;
+    NSLog(@"Length is %ld", (long)length);
+//The numberFromString: method from the NSNumberFormatter class, as if the number is not valid, it will return nil; otherwise, it will return you an NSNumber.
+    NSNumberFormatter *nf = [[NSNumberFormatter alloc] init];
+    BOOL isDecimal = [nf numberFromString:input];
+    if (isDecimal) {
+        NSLog(@"The string is decimal: %@", input);
+    }
+    else NSLog(@"The string is NOT decimal: %@", input);
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
